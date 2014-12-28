d2c6d48e9db2f6e83e2b6cb273bfe785a68bf14c
Andy Uyeda <AndyUyeda@Andys-MacBook-Pro.local>
2014-12-27 22:49:16 -0600

    Initial Commit

---
+++ Legacy/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Legacy
+//
+//  Created by Andy Uyeda on 12/27/14.
+//  Copyright (c) 2014 Andy Uyeda. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
